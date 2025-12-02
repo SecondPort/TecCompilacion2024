b06ee14609e18738672b30b61c20ca5134d82e36
SecondPort <gomezmanulm@gmail.com>
2025-12-02 10:44:59 +0000

    Add initial implementation of corrected program and assembly output
    - Created `programa_corregido.txt` with basic arithmetic operations and loops.
    - Introduced `programa.asm` with corresponding assembly code for variable initialization and loop structures.
    - Added binary output files for the compiled program and object code.

--- salida/programa.asm
+++ salida/programa.asm
@@ -3,9 +3,11 @@ section .bss
     x: resd 1  ; int
     y: resd 1  ; int
     z: resd 1  ; int
+    b: resd 1  ; int
     j: resd 1  ; int
+    suma: resd 1  ; int
     i: resd 1  ; int
-    i: resd 1  ; int
+    k: resd 1  ; int
     l: resd 1  ; int
 
 ; Código ensamblador generado automáticamente
@@ -22,6 +24,8 @@ _start:
     mov dword [x], 1
     ; Inicialización de y
     mov dword [y], 3
+    ; Inicialización de b
+    mov dword [b], 5
 
     ; Asignación a x
     mov eax, 1
@@ -49,52 +53,89 @@ _start:
     pop eax
     add eax, ebx  ; suma
     mov [z], eax
+    ; Inicialización de j
+    mov dword [j], 0
+    ; Inicialización de suma
+    mov dword [suma], 0
 
     ; Estructura FOR
     ; Inicialización de i
     mov dword [i], 0
+L0:
     mov eax, [i]
     push eax
     mov eax, 10
     mov ebx, eax
     pop eax
     cmp eax, ebx  ; comparación
-L0:
     je L1  ; salir si falso
     ; Inicio de bloque
+
+    ; Asignación a suma
+    mov eax, [suma]
+    push eax
+    mov eax, [i]
+    mov ebx, eax
+    pop eax
+    add eax, ebx  ; suma
+    mov [suma], eax
     ; Fin de bloque
+    ; Incremento de i
+    inc dword [i]
     jmp L0  ; repetir bucle
 L1:
 
     ; Estructura FOR
-    ; Inicialización de i
-    mov dword [i], 0
-    mov eax, [i]
+    ; Inicialización de k
+    mov dword [k], 0
+L2:
+    mov eax, [k]
     push eax
-    mov eax, 10
+    mov eax, 5
     mov ebx, eax
     pop eax
     cmp eax, ebx  ; comparación
-L2:
     je L3  ; salir si falso
     ; Inicio de bloque
+
+    ; Asignación a j
+    mov eax, [j]
+    push eax
+    mov eax, 1
+    mov ebx, eax
+    pop eax
+    add eax, ebx  ; suma
+    mov [j], eax
     ; Fin de bloque
+    ; Incremento de k
+    inc dword [k]
     jmp L2  ; repetir bucle
 L3:
 
     ; Estructura FOR
     ; Inicialización de l
     mov dword [l], 0
+L4:
     mov eax, [l]
     push eax
-    mov eax, 10
+    mov eax, 3
     mov ebx, eax
     pop eax
     cmp eax, ebx  ; comparación
-L4:
     je L5  ; salir si falso
     ; Inicio de bloque
+
+    ; Asignación a x
+    mov eax, [x]
+    push eax
+    mov eax, 1
+    mov ebx, eax
+    pop eax
+    add eax, ebx  ; suma
+    mov [x], eax
     ; Fin de bloque
+    ; Incremento de l
+    inc dword [l]
     jmp L4  ; repetir bucle
 L5:
 
